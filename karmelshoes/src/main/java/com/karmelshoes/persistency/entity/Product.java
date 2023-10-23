6a8dda6d88ee19aba7196e423e3d7a62c3054b34
Juan Pablo <fonseca123papo@gmail.com>
2023-10-23 08:46:46 -0500

    creando clases de shoes y product

---
+++ karmelshoes/src/main/java/com/karmelshoes/persistency/entity/Product.java
@@ -0,0 +1,77 @@
+package com.karmelshoes.persistency.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private Double price;
+    @Column(name = "quantity")
+    private Integer quantity;
+    @Column(name = "product_type")
+    private String productType;
+
+    public Long getIdShoes() {
+        return id;
+    }
+
+    public void setIdShoes(Long idShoes) {
+        this.id = idShoes;
+    }
+
+    public Product() {
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ karmelshoes/src/main/java/com/karmelshoes/persistency/entity/ShoesEntity.java
@@ -0,0 +1,68 @@
+package com.karmelshoes.persistency.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "shoes")
+public class ShoesEntity extends Product{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long idShoes;
+
+    @Column(name = "mark")
+    private String mark;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "color")
+    private String color;
+
+    public Long getIdShoes() {
+        return idShoes;
+    }
+
+    public void setIdShoes(Long idShoes) {
+        this.idShoes = idShoes;
+    }
+
+    public ShoesEntity() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+}
