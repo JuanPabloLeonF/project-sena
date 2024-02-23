d312a625da25bf64a75e345a1920f293b558b1a2
Juan Pablo <fonseca123papo@gmail.com>
2024-02-23 16:51:16 -0500

    mejorando el diseño del main

--- karmelshoes-frontend/src/components/senaApp/Main.jsx
+++ karmelshoes-frontend/src/components/senaApp/Main.jsx
@@ -1,11 +1,20 @@
 import "/src/css/styleMain.css"
 
 export const Main = () => {
+
+
+
   return (
     <>
       <main className="main">
-        <img src="/src/assets/imgs/captura descuento 2023-11-17 170940.png" alt="Imagen de promociones"/>
-        <img src="/src/assets/imgs/logo karmel shoes 2023-11-17 171220.png" alt="Imagen de la empresa" />
+        <div className="gallery-container">
+          <img className="gallery-item-1" src="/src/assets/imgs/fondo_page_2.jpg" alt="imgagen de muestra" data-index="1" />
+          <img className="gallery-item-2" src="/src/assets/imgs/fondo_page_1.jpg" alt="imgagen de muestra" data-index="2" />
+          <img className="gallery-item-3" src="/src/assets/imgs/fondo_page_3.jpg" alt="imgagen de muestra" data-index="3" />
+          <img className="gallery-item-4" src="/src/assets/imgs/fondo_page_4.jpg" alt="imgagen de muestra" data-index="4" />
+          <img className="gallery-item-5" src="/src/assets/imgs/fondo_page_5.jpg" alt="imgagen de muestra" data-index="5" />
+        </div>
+        <div className="gallery-controls"></div>
       </main>
     </>
   );
